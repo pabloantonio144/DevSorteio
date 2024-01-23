db3245938f591404e622b5480f987c4f1c1cf037
pabloantonio144 <pablo14@gmail.com>
2024-01-23 18:47:36 -0500

    adicionado regra

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
+    if(min >= max){
+        alert('O valor minimo tem que ser menor que o valor maximo')
+    }else{
+
     const result = Math.floor(Math.random() * (max - min +1)) + min;
 
     alert(result)
+}
 }
