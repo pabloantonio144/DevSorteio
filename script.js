cf530c61c86b152e7fa211dd6fed7b5763d0ef60
pabloantonio144 <pablo14@gmail.com>
2024-02-13 23:15:43 -0500

    atualizações

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function generateNumber(){
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
+    let h2 = document.querySelector('h2')
 
     if(min >= max){
         alert('O valor minimo tem que ser menor que o valor maximo')
@@ -8,6 +9,7 @@ function generateNumber(){
 
     const result = Math.floor(Math.random() * (max - min +1)) + min;
 
-    alert(result)
+    h2.innerHTML = result
+    
 }
 }
